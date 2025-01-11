a97d36f82ea2e8e99913e7d57f892e2929548988
Cathy Ngo <cathy.x86@gmail.com>
2025-01-11 15:46:52 +0800

    Add skills label

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -8,10 +8,11 @@ export default function Skills() {
             <div className="flex justify-center">
                 <h2 className="jua gradient-text text-4xl mb-8">skills</h2>
             </div>
-            <div className="grid grid-cols-4 gap-8 mx-auto w-fit">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 mx-auto w-fit">
                 {SKILLS.map((skill) => (
-                    <div key={skill.name} className="flex items-center justify-center opaque-white rounded aspect-square w-40 h-40">
-                        <img src={`${filepath + skill.filename}`} alt={`${skill.name} icon`} className="w-28 h-28 object-contain"/>
+                    <div key={skill.name} className="flex flex-col gap-3 pt-2 items-center justify-center opaque-white rounded aspect-square w-36 h-36">
+                        <img src={`${filepath + skill.filename}`} alt={`${skill.name} icon`} className="w-20 h-20 object-contain"/>
+                        <div className="gaegu text-m text-brownie">{skill.name}</div>
                     </div>
                 ))}
             </div>
