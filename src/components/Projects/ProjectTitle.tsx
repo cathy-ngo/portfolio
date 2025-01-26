08df32144bb4b4ef28a220ebac1189ee4deb22a4
Cathy <cathy.x86@gmail.com>
2025-01-26 20:18:51 +0800

    Update manifest theme colours

--- src/components/Projects/ProjectTitle.tsx
+++ src/components/Projects/ProjectTitle.tsx
@@ -1,12 +1,12 @@
-type ProjectTitleProps = { 
-    title: string 
+type ProjectTitleProps = {
+    title: string
 };
 
 export default function ProjectTitle({ title }: ProjectTitleProps) {
     return (
         <div className="flex gap-2 w-max bg-brownie px-3 rounded">
             <img src="icons/bunny.svg" alt="Bunny" className="w-4"></img>
-            <h3>{title}</h3>
+            <h3 className="jua text-2xl text-vanilla">{title}</h3>
         </div>
     );
 }
